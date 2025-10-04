566de21a9441df0306a21b4d1740798727f3e2aa
Vinay730 <vinay.juris@gmail.com>
2025-10-04 11:51:09 +0100

    changes

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import HeroAbout from '../components/aboutcomponents/HeroAbout'
-import OurMission from '../components/aboutcomponents/OurMission'
-import WhyChooseUs from '../components/aboutcomponents/WhyChooseUs'
-import WhoWeAre from '../components/aboutcomponents/WhoWeAre'
-import WhatWeDo from '../components/aboutcomponents/WhatWeDo'
-import CTA from '../components/aboutcomponents/CTA'
-
+import React from "react";
+import HeroAbout from "../components/aboutcomponents/HeroAbout";
+import OurMission from "../components/aboutcomponents/OurMission";
+import WhyChooseUs from "../components/aboutcomponents/WhyChooseUs";
+import WhoWeAre from "../components/aboutcomponents/WhoWeAre";
+import WhatWeDo from "../components/aboutcomponents/WhatWeDo";
+import CTA from "../components/aboutcomponents/CTA";
+import AboutUsSection from "../components/homeComponents/AboutUsSection";
 
 const AboutPage = () => {
   return (
     <>
-    <HeroAbout/>
-    <OurMission/>
-    <WhyChooseUs/>
-    <WhoWeAre/>
-    <WhatWeDo/>
-    <CTA/>
+      <HeroAbout />
+      <AboutUsSection />
+      <OurMission />
+      <WhyChooseUs />
+      <WhoWeAre />
+      <WhatWeDo />
+      <CTA />
     </>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
